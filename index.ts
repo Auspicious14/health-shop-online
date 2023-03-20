1074288663ea78d412ab5076126cc32367e6038e
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2023-03-20 15:10:08 +0100

    configure app

---
+++ index.ts
@@ -0,0 +1,17 @@
+import router from "./routes/userAuth";
+const cookieParser = require("cookie-parser");
+import express from "express";
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 5000;
+const URI = process.env.MONGODB_URL;
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(port, () => console.log(`server is listening on port ${port}`))
+  );
+app.use(cookieParser());
+app.use(express.json());
+app.use("/auth", router);
