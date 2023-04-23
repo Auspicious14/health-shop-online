225a958f1b0b7652f36288b10076d9c01e73eac3
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2023-04-23 18:24:14 +0100

    added catch block to mongoose connection

--- server.ts
+++ server.ts
@@ -11,6 +11,7 @@ mongoose
   .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(port, () => console.log(`server is listening on port ${port}`))
-  );
+  )
+  .catch((err: any) => console.log(err));
 
 app.use(appRoute);
