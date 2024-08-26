8b9d5299234cd6a6c81ab383f5aa02863e0431f5
Auspicious14 <uthmanabdulganiyu2019@gmail.com>
2024-08-26 23:43:44 +0100

    [update] chat mechanism

--- routes/chat.ts
+++ routes/chat.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { getMessages, sendMessage } from "../controllers/chat/chat";
+import {
+  getMessagesBetweenUserAndStore,
+  getUsersWhoMessagedStore,
+  sendMessage,
+} from "../controllers/chat/chat";
 
 const router = express.Router();
 
 router.post("/chat", sendMessage);
-router.get("/chat", getMessages);
+router.get("/store-user-messages", getMessagesBetweenUserAndStore);
+router.get("/store-users-chat", getUsersWhoMessagedStore);
 
 export default router;
